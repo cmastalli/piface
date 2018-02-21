cefbca6688fe74d72d2b2d79f4a096f618efbb35
Carlos Mastalli <carlos.mastalli@iit.it>
2018-02-22 00:10:31 +0100

    [opt] By default framePosition updates the kinematics, this isn't useful because forwardKinematics already done

--- fkin.py
+++ fkin.py
@@ -1,4 +1,5 @@
 import pinocchio as pin
+import numpy as np
 
 
 class fkin:
@@ -9,19 +10,22 @@ class fkin:
     pin.forwardKinematics(self.robot.model, self.robot.data, self.robot.q0)
     
     frame_id = self.robot.model.getFrameId('lf_foot')
-    print 'lf_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id).translation.T
+    print 'lf_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id, False).translation.T
 
     frame_id = self.robot.model.getFrameId('lh_foot')
-    print 'lh_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id).translation.T
+    print 'lh_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id, False).translation.T
 
     frame_id = self.robot.model.getFrameId('rf_foot')
-    print 'rf_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id).translation.T
+    print 'rf_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id, False).translation.T
 
     frame_id = self.robot.model.getFrameId('rh_foot')
-    print 'rh_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id).translation.T
+    print 'rh_foot:', self.robot.robot.framePosition(self.robot.q0, frame_id, False).translation.T
+    f = self.robot.model.frames[frame_id]
+    print '        ', self.robot.data.oMi[f.parent].act(f.placement).translation.T
 
 
 
+np.set_printoptions(suppress=True)
 from hyq import HyQ
 hyq = HyQ()
 
