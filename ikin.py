6c9b97ded82a263da3703e241eb969f25c646547
Carlos Mastalli <carlos.mastalli@iit.it>
2018-02-22 00:11:02 +0100

    [wip] Declared classes for IK and Jacobians

---
+++ ikin.py
@@ -0,0 +1,9 @@
+
+
+
+
+class ikin:
+  def __init__(self, robot):
+    self.robot = robot
+    
+  def compute(self):
---
+++ jac.py
@@ -0,0 +1,23 @@
+import pinocchio as pin
+import numpy as np
+
+
+class jac:
+  def __init__(self, robot):
+    self.robot = robot
+    
+  def compute(self):
+    frame_id = self.robot.model.getFrameId('lh_foot')
+    print pin.frameJacobian(self.robot.model, self.robot.data, frame_id, self.robot.q0)
+    print '---'
+    print pin.computeJacobians(self.robot.model, self.robot.data, self.robot.q0)
+    print '---'
+    print pin.se3.jacobian(self.robot.model, self.robot.data, self.robot.q0, frame_id, False, True) #False == world
+
+
+np.set_printoptions(suppress=True)
+from hyq import HyQ
+hyq = HyQ()
+
+j = jac(hyq)
+j.compute()
